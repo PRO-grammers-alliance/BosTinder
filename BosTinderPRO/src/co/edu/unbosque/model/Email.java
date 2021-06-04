ed7cb0bcfdc107f8ebb3189b7b032d11e4b53e8e
Santiago Flórez <Santiago Flrez@DESKTOP-7MPV2M3>
2021-06-04 09:45:31 -0500

    No se hizo nada

--- BosTinderPRO/src/co/edu/unbosque/model/Email.java
+++ BosTinderPRO/src/co/edu/unbosque/model/Email.java
@@ -1,12 +1,14 @@
 package co.edu.unbosque.model;
 
 import java.util.Properties;
+
 import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+
 
 public class Email {
 
