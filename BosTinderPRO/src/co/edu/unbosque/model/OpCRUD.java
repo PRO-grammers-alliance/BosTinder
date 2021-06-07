e2c81910f1503d1c6f07c58935a292f3a5ad12b6
Juliana Flórez <Juliana Flrez@LAPTOP-UM6D44HL>
2021-06-07 14:32:28 -0500

    Configuracion de filtros de forma ascendente, ya casi

--- BosTinderPRO/src/co/edu/unbosque/model/OpCRUD.java
+++ BosTinderPRO/src/co/edu/unbosque/model/OpCRUD.java
@@ -90,14 +90,14 @@ public class OpCRUD {
 				gen += edad.get(i) + " -> " + nombre.get(i) + " -> " + apellido1.get(i) + " -> " + apellido2.get(i)
 						+ " -> " + alias.get(i) + " -> " + numlikesR.get(i) + " -> " + numlikesO.get(i) + " -> "
 						+ ingresos.get(i) + " -> " + genero.get(i) + "\n";
-			}else if (genero.get(i).equals("M") && tipo.equals("Alias")) {
+			} else if (genero.get(i).equals("M") && tipo.equals("Alias")) {
 				gen += alias.get(i) + " -> " + nombre.get(i) + " -> " + apellido1.get(i) + " -> " + apellido2.get(i)
-				+ " -> " + edad.get(i) + " -> " + numlikesR.get(i) + " -> " + numlikesO.get(i) + " -> "
-				+ ingresos.get(i) + " -> " + genero.get(i) + "\n";
-			}else if (genero.get(i).equals("M") && tipo.equals("Likes")) {
+						+ " -> " + edad.get(i) + " -> " + numlikesR.get(i) + " -> " + numlikesO.get(i) + " -> "
+						+ ingresos.get(i) + " -> " + genero.get(i) + "\n";
+			} else if (genero.get(i).equals("M") && tipo.equals("Likes")) {
 				gen += numlikesR.get(i) + " -> " + numlikesO.get(i) + " -> " + nombre.get(i) + " -> " + apellido1.get(i)
-				+ " -> " + apellido2.get(i) + " -> " + edad.get(i) + " -> " + alias.get(i) + " -> "
-				+ ingresos.get(i) + " -> " + genero.get(i) + "\n";
+						+ " -> " + apellido2.get(i) + " -> " + edad.get(i) + " -> " + alias.get(i) + " -> "
+						+ ingresos.get(i) + " -> " + genero.get(i) + "\n";
 			}
 			i++;
 		}
@@ -122,14 +122,14 @@ public class OpCRUD {
 				gen += edad.get(i) + " -> " + nombre.get(i) + " -> " + apellido1.get(i) + " -> " + apellido2.get(i)
 						+ " -> " + alias.get(i) + " -> " + numlikesR.get(i) + " -> " + numlikesO.get(i) + " -> "
 						+ ingresos.get(i) + " -> " + genero.get(i) + "\n";
-			}else if (genero.get(i).equals("H") && tipo.equals("Alias")) {
+			} else if (genero.get(i).equals("H") && tipo.equals("Alias")) {
 				gen += alias.get(i) + " -> " + nombre.get(i) + " -> " + apellido1.get(i) + " -> " + apellido2.get(i)
-				+ " -> " + edad.get(i) + " -> " + numlikesR.get(i) + " -> " + numlikesO.get(i) + " -> "
-				+ ingresos.get(i) + " -> " + genero.get(i) + "\n";
-			}else if (genero.get(i).equals("H") && tipo.equals("Likes")) {
+						+ " -> " + edad.get(i) + " -> " + numlikesR.get(i) + " -> " + numlikesO.get(i) + " -> "
+						+ ingresos.get(i) + " -> " + genero.get(i) + "\n";
+			} else if (genero.get(i).equals("H") && tipo.equals("Likes")) {
 				gen += numlikesR.get(i) + " -> " + numlikesO.get(i) + " -> " + nombre.get(i) + " -> " + apellido1.get(i)
-				+ " -> " + apellido2.get(i) + " -> " + edad.get(i) + " -> " + alias.get(i) + " -> "
-				+ ingresos.get(i) + " -> " + genero.get(i) + "\n";
+						+ " -> " + apellido2.get(i) + " -> " + edad.get(i) + " -> " + alias.get(i) + " -> "
+						+ ingresos.get(i) + " -> " + genero.get(i) + "\n";
 			}
 			i++;
 		}
@@ -140,58 +140,66 @@ public class OpCRUD {
 			ArrayList<String> apellido2, ArrayList<Integer> edad, ArrayList<String> alias, ArrayList<Integer> numlikesR,
 			ArrayList<Integer> numlikesO, ArrayList<Double> ingresos) {
 		String des = "";
-		Collections.sort(nombre);
-		Collections.sort(apellido1);
-		Collections.sort(apellido2);
-		Collections.sort(edad);
-		Collections.sort(alias);
-		Collections.sort(numlikesR);
-		Collections.sort(numlikesO);
-		Collections.sort(ingresos);
+		// Collections.sort(apellido1);
+		ArrayList<String> nom = new ArrayList<>();
+		/*
+		 * Collections.sort(apellido2); Collections.sort(edad); Collections.sort(alias);
+		 * Collections.sort(numlikesR); Collections.sort(numlikesO);
+		 * Collections.sort(ingresos);
+		 */
 		if (tipo.equals("Nombre")) {
 			int i = 0;
 			while (i < nombre.size()) {
-				des += nombre.get(i);
+				des = nombre.get(i) + "  " + apellido1.get(i) + "  " + apellido2.get(i) + "  " + edad.get(i) + "  "
+						+ alias.get(i) + "  " + numlikesR.get(i) + "  " + ingresos.get(i) + "\n";
+				nom.add(des);
+				Collections.sort(nom);
 				i++;
 			}
 		} else if (tipo.equals("Apellido")) {
 			int i = 0;
 			while (i < apellido1.size()) {
-				des += "Apellido 1 ";
-				des += apellido1.get(i) + "\n";
-				des += "Apellido 2 ";
-				des += apellido2.get(i) + "\n";
+				des = apellido1.get(i) + "  " + apellido2.get(i) + "  " + nombre.get(i) + " " + edad.get(i) + "  "
+						+ alias.get(i) + "  " + numlikesR.get(i) + "  " + ingresos.get(i) + "\n";
+				nom.add(des);
+				Collections.sort(nom);
 				i++;
 			}
 		} else if (tipo.equals("Edad")) {
 			int i = 0;
 			while (i < edad.size()) {
-				des += edad.get(i) + "\n";
+				des = edad.get(i) + "  " + nombre.get(i) + "  " + apellido1.get(i) + "  " + apellido2.get(i) + "  "
+						+ alias.get(i) + "  " + numlikesR.get(i) + "  " + ingresos.get(i) + "\n";
+				nom.add(des);
+				Collections.sort(nom);
 				i++;
 			}
 		} else if (tipo.equals("Alias")) {
 			int i = 0;
 			while (i < alias.size()) {
-				des += alias.get(i) + " -> " + nombre.get(i) + "\n";
+				des = alias.get(i) + "  " + nombre.get(i) + "  " + apellido1.get(i) + "  " + apellido2.get(i) + "  "
+						+ edad.get(i) + "  " + numlikesR.get(i) + "  " + ingresos.get(i) + "\n";
+				nom.add(des);
+				Collections.sort(nom);
 				i++;
 			}
 		} else if (tipo.equals("Likes")) {
 			int i = 0;
 			while (i < numlikesR.size()) {
-				des += "Likes Recibidos ";
-				des += numlikesR.get(i) + "\n";
-				des += "Likes Otorgados ";
-				des += numlikesO.get(i) + "\n";
-				i++;
-			}
-		} else if (tipo.equals("ingresos")) {
-			int i = 0;
-			while (i < ingresos.size()) {
-				des += ingresos.get(i);
+				des = numlikesR.get(i) + "  " + nombre.get(i) + "  " + apellido1.get(i) + "  " + apellido2.get(i) + "  "
+						+ edad.get(i) + "  " + alias.get(i) + "  " + ingresos.get(i) + "\n";
+				nom.add(des);
+				Collections.sort(nom);
 				i++;
 			}
 		}
-		return des;
+		int j = 0;
+		String g = "";
+		while (j < nombre.size()) {
+			g += nom.get(j);
+			j++;
+		}
+		return g;
 	}
 
 	public String descendente(String tipo, ArrayList<String> nombre, ArrayList<String> apellido1,
